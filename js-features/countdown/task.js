2701bf87d76d24cb844614b6950487830ffec47e
Konstantin Zatyura <19kostik94@mail.ru>
2023-06-28 00:37:17 +0700

    правка первого задания

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,12 +1,10 @@
-const endTimer = function() {
+const endTimer = setInterval(function() {
     const time = document.getElementById("timer");
     
     if (time.textContent > 0) {
         time.textContent -= 1;
     } else {
         alert ("Вы победили в конкурсе!");
-        time.textContent = 59;
+        clearInterval(endTimer);
     }
-};
-
-setInterval(endTimer, 1000)
+}, 1000)
