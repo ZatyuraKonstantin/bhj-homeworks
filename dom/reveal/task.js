d73dbda2b975e1460f9b0b25987b0f01cd5468df
Konstantin Zatyura <19kostik94@mail.ru>
2023-07-18 00:18:03 +0700

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,22 @@
+const block = document.getElementsByClassName('reveal');
+
+let isInViewPort = function(element) {
+    const viewPortHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop < 0 && elementTop > viewPortHeight) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+window.addEventListener('scroll', function () {
+    for (let i = 0; i < block.length; i++) {
+        if ( isInViewPort(block[i]) === true ) {
+            block[i].classList.add('reveal_active');
+        } else {
+            block[i].classList.remove('reveal_active');
+        }
+    }
+});
