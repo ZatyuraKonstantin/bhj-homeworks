18a4424e8dd5e89273f66ccd1e900814f10549e7
Konstantin Zatyura <19kostik94@mail.ru>
2023-07-30 00:22:46 +0700

    second commit preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,32 +1,28 @@
 const items = document.getElementById('items');
-const loader = document.getElementById('loader')
+const loader = document.getElementById('loader');
 
 const xhr = new XMLHttpRequest( );
 xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/slow-get-courses", true);
+xhr.responseType = "json";
+xhr.onload = () => {
+  let valute = xhr.response.response.Valute;
+  for (let i in valute){
+      items.insertAdjacentHTML("afterbegin", `
+      <div class="item__code">
+      ${valute[i].CharCode}
+              </div>
+              <div class="item__value">
+                  ${valute[i].Value}
+              </div>
+              <div class="item__currency">
+                  руб.
+              </div>`)
+  }
+  animation.classList.remove("loader_active");
+}
 xhr.send();
 
-xhr.addEventListener("readystatechange", () => {
-    if (xhr.readyState === xhr.DONE && xhr.statusText === "OK") {
-      loader.classList.remove('loader_active');
-  
-      items.textContent = "";
-      data = JSON.parse(xhr.response).response.Valute;
-      localStorage.setItem("data", JSON.stringify(data));
 
-      for (let key in data) {
-        items.innerHTML += `
-        <div class="item">
-          <div class = "item__code">
-            ${data[key].CharCode}
-          </div>
-          <div class = "item__value">
-            ${data[key].Value}
-          </div>
-          <div class = "item__currency">
-            руб.
-          </div>
-        </div>
-        `;
-      }
-    }
-});
+
+
+
